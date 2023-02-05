62782822c072bfab83a2f420cda063f9afa832ac
Ange03Leon01 <ange03leon01@gmail.com>
2023-02-04 23:22:06 -0600

    Crear las clases model, mapeo de clases a tablas con JPA, integrado la parte de diseño y vistas

--- src/main/java/com/curso/ecommerce/springecommerce/model/DetalleOrden.java
+++ src/main/java/com/curso/ecommerce/springecommerce/model/DetalleOrden.java
@@ -1,15 +1,42 @@
 package com.curso.ecommerce.springecommerce.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "detalles")
 public class DetalleOrden {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private double cantidad;
 	private double precio;
 	private double Total;
 
+	@OneToOne
+	private Orden orden;
+	
+	@ManyToOne
+	private Producto productos;
+	
 	public DetalleOrden() {
 		
+	}
+
+	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		Total = total;
 	}
 
 	public Integer getId() {
@@ -50,6 +77,22 @@ public class DetalleOrden {
 
 	public void setTotal(double total) {
 		Total = total;
+	}	
+
+	public Orden getOrden() {
+		return orden;
+	}
+
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
+
+	public Producto getProductos() {
+		return productos;
+	}
+
+	public void setProductos(Producto productos) {
+		this.productos = productos;
 	}
 
 	@Override
