62f396f4833b2a23c46596192d0f3a7a6cb90804
Ange03Leon01 <ange03leon01@gmail.com>
2023-02-13 20:46:35 -0600

    Metodo para generar el numero de la orden.

--- src/main/java/com/curso/ecommerce/springecommerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecommerce/springecommerce/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package com.curso.ecommerce.springecommerce.service;
 
+import java.util.List;
+
 import com.curso.ecommerce.springecommerce.model.Orden;
 	
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden Save(Orden orden);
 }
