eaa2aafb3ba8944de92a7fc46f851d459af84ec6
Ange03Leon01 <ange03leon01@gmail.com>
2023-02-25 23:37:05 -0600

    Funcionalidad para ver los detalles de las ordenes desde el admin

--- src/main/java/com/curso/ecommerce/springecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/springecommerce/controller/AdministradorController.java
@@ -2,13 +2,19 @@ package com.curso.ecommerce.springecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.springecommerce.model.Orden;
 import com.curso.ecommerce.springecommerce.model.Producto;
+import com.curso.ecommerce.springecommerce.service.IDetalleOrdenService;
+import com.curso.ecommerce.springecommerce.service.IOrdenService;
 import com.curso.ecommerce.springecommerce.service.IUsuarioService;
 import com.curso.ecommerce.springecommerce.service.ProductoService;
 
@@ -21,6 +27,14 @@ public class AdministradorController {
 	
 	@Autowired
 	private IUsuarioService usuarioService;
+	
+	@Autowired
+	private IOrdenService ordenService;
+	
+	@Autowired
+	private IDetalleOrdenService detalleOrdenService;
+	
+	private final Logger logg = LoggerFactory.getLogger(AdministradorController.class);
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -36,4 +50,18 @@ public class AdministradorController {
 		return "administrador/usuarios";
 	}
 	
+	@GetMapping("/ordenes")
+	public String ordenes(Model model) {
+		List<Orden> ordenes = ordenService.findAll();
+		model.addAttribute("ordenes", ordenes);
+		return "administrador/ordenes";
+	}
+	
+	@GetMapping("/detalleorden/{id}")
+	public String detalleOrden(@PathVariable Integer id, Model model) {
+		logg.info("id de la orden: {}", id);
+		Orden orden = ordenService.findById(id).get();
+		model.addAttribute("detallesordenes", orden.getDetalle());
+		return "administrador/detalleorden";
+	}
 }
--- src/main/java/com/curso/ecommerce/springecommerce/service/IDetalleOrdenService.java
+++ src/main/java/com/curso/ecommerce/springecommerce/service/IDetalleOrdenService.java
@@ -3,5 +3,6 @@ package com.curso.ecommerce.springecommerce.service;
 import com.curso.ecommerce.springecommerce.model.DetalleOrden;
 
 public interface IDetalleOrdenService {
+
 	DetalleOrden save(DetalleOrden detalleOrden);
 }
